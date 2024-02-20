c0b214a1fa5f718f43ebd20209e64050defacc83
Shubham Shinde <shubham.shinde@perforce.com>
2024-02-20 15:54:14 +0530

    (PA-6002) Add SLES-11 (Intel) support  - Add SLES-11 (Intel) platform definition file  - SLES-11 uses pl-autotools instead of automake and autoconf since puppet7 uses ruby 2 which is not compatible with the supported version of autoconf and automake for SLES-11.  - Make cpp-pcp-client, leatherman and pxp-agent components to use the default pl-build-tools.  - SLES-11 does not require the runtime component since it only adds pl-gcc for it which we already install in its platform definition file.  - Due to compatibility reasons pl-gcc and pl-cmake have been manually downgraded in the platform definition file.

---
+++ configs/platforms/sles-11-x86_64.rb
@@ -0,0 +1,24 @@
+platform "sles-11-x86_64" do |plat|
+  plat.servicedir "/etc/init.d"
+  plat.defaultdir "/etc/sysconfig"
+  plat.servicetype "sysv"
+
+  plat.add_build_repository "http://osmirror.delivery.puppetlabs.net/sles-11-deps-x86_64/sles-11-deps-x86_64.repo"
+  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/sles/11/x86_64/pl-build-tools-sles-11-x86_64.repo"
+  packages = %w[
+    aaa_base
+    libbz2-devel
+    make
+    pkgconfig
+    pl-autotools
+    pl-cmake
+    readline-devel
+    rsync
+    zlib-devel
+  ]
+  plat.install_build_dependencies_with "zypper -n --no-gpg-checks install -y"
+  plat.provision_with("zypper -n --no-gpg-checks install -y #{packages.join(' ')}")
+  plat.provision_with("zypper install -y --oldpackage pl-gcc=4.8.2-1")
+  plat.provision_with "zypper install -y --oldpackage pl-cmake-3.2.3-13.sles11.x86_64"
+  plat.vmpooler_template "sles-11-x86_64"
+end
